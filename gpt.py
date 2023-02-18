37e03a49d30d117e97a7ba9125e3dc5cf269a5fd
ethan_leong <6923102@qq.com>
2023-02-18 10:14:36 +0800

    update

--- gpt.py
+++ gpt.py
@@ -1,20 +1,29 @@
-import sys
 import os
 import openai
 
 openai.api_key = os.environ['OPEN_AI_KEY']
-prompt = sys.argv[1]
 
 
-def ask(question: str):
+def ask():
+    next_answer = input('我: ')
+    print('Waiting...')
     response = openai.Completion.create(
-        engine="text-davinci-003", prompt=question, temperature=0.3, top_p=1, frequency_penalty=0.5, max_tokens=1024)
+        engine="text-davinci-003",
+        prompt=next_answer,
+        temperature=0.3,
+        top_p=1,
+        frequency_penalty=0.5,
+        max_tokens=1024,
+        stop=None)
 
+    print('GPT：')
+    # print(response)
     for res in response.choices:
         print(res.text)
-        print('==========end=========')
+    print('End...')
+
+    ask()
 
 
 if __name__ == '__main__':
-    print('Waiting...')
-    ask(prompt)
+    ask()
